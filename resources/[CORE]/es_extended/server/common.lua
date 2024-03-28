c67eb349c84c3af3f5784944feb226d4a73c9a46
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-03-28 23:44:12 +0100

    Fix invalid invoking resource on esx:getSharedObject listener

--- resources/[CORE]/es_extended/server/common.lua
+++ resources/[CORE]/es_extended/server/common.lua
@@ -26,7 +26,7 @@ AddEventHandler("esx:getSharedObject", function(cb)
 		cb(ESX);
 		return;
 	end
-    print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
+    print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(invoke))
 end)
 
 exports("getSharedObject", function()
